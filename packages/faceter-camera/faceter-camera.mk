1f7519d9f0399e0ca6ab0ac6fc7ccc1b50e1f940
Marat Iskhakov <marat.iskakov@avalab.io>
2024-01-29 13:39:02 +0500

    build from archive

--- packages/faceter-camera/faceter-camera.mk
+++ packages/faceter-camera/faceter-camera.mk
@@ -5,8 +5,9 @@
 ################################################################################
 
 FACETER_CAMERA_VERSION = 1.2.1
+FACETER_CAMERA_SOURCE = faceter-camera.tar.gz
 FACETER_CAMERA_SITE = $(TOPDIR)/../../../..
-FACETER_CAMERA_SITE_METHOD = local
+FACETER_CAMERA_SITE_METHOD = file
 
 FACETER_CAMERA_DEPENDENCIES = \
 	host-pkgconf \
