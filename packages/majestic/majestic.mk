8f421b68d544ca57fadb805552dbc6139c6db6e9
Marat Iskhakov <marat.iskakov@avalab.io>
2024-02-01 02:11:28 +0500

    override packages

---
+++ packages/majestic/majestic.mk
@@ -0,0 +1,52 @@
+################################################################################
+#
+# majestic
+#
+################################################################################
+
+MAJESTIC_SITE = $(MAJESTIC_PKGDIR)/files
+MAJESTIC_SOURCE = majestic.$(MAJESTIC_FAMILY).$(MAJESTIC_RELEASE).master.tar.bz2
+MAJESTIC_SITE_METHOD = file
+
+MAJESTIC_LICENSE = PROPRIETARY
+MAJESTIC_LICENSE_FILES = LICENSE
+
+MAJESTIC_FAMILY = $(OPENIPC_SOC_FAMILY)
+MAJESTIC_RELEASE = $(OPENIPC_FLAVOR)
+
+# we don't have Majestic ultimate for these platforms
+MAJESTIC_LIST = hi3516av100 hi3519v101
+
+ifneq ($(filter $(MAJESTIC_LIST),$(MAJESTIC_FAMILY)),)
+	MAJESTIC_RELEASE = lite
+endif
+
+ifeq ($(MAJESTIC_RELEASE),lte)
+	MAJESTIC_RELEASE = fpv
+endif
+
+MAJESTIC_DEPENDENCIES = \
+	libevent-openipc \
+	libogg-openipc \
+	mbedtls-openipc \
+	opus-openipc \
+	json-c \
+	zlib
+
+MAJESTIC_STRIP_COMPONENTS = 0
+
+define MAJESTIC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc
+	$(INSTALL) -m 644 $(@D)/majestic-mini.yaml $(TARGET_DIR)/etc/majestic.yaml
+	$(INSTALL) -m 644 $(@D)/majestic.yaml $(TARGET_DIR)/etc/majestic.full
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/init.d
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(MAJESTIC_PKGDIR)/files/S95majestic
+
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/majestic
+
+	rm -rf $(MAJESTIC_DL_DIR)
+endef
+
+$(eval $(generic-package))
---
+++ packages/quirc-openipc/quirc-openipc.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# quirc-openipc
+#
+################################################################################
+
+QUIRC_OPENIPC_SITE = https://github.com/openipc/quirc/archive
+QUIRC_OPENIPC_SOURCE = master.tar.gz
+
+QUIRC_OPENIPC_DEPENDENCIES = libjpeg
+QUIRC_OPENIPC_MAKE_OPTS = \
+	CC=$(TARGET_CC) \
+	AR=$(TARGET_AR) \
+	LDFLAGS="-s"
+
+define QUIRC_OPENIPC_BUILD_CMDS
+	$(MAKE) $(QUIRC_OPENIPC_MAKE_OPTS) -C $(@D) all
+endef
+
+define QUIRC_OPENIPC_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 755 -d $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/qrscan
+endef
+
+$(eval $(generic-package))
